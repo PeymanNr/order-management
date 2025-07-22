e4492002b094f1ba9d60bdfa0bf669e19a6f8cd1
PeymanNr <peyman.naseri7@gmail.com>
2025-07-22 12:25:23 +0330

    feat(auth): added supersuser and updated project settings

--- account/models.py
+++ account/models.py
@@ -1,7 +1,26 @@
+from django.contrib.auth.models import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        if not username:
+            raise ValueError("The Username must be set")
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('role', 'ADMIN')
+        return self.create_user(username, email, password, **extra_fields)
+
+
 class User(AbstractUser):
     class Roles(models.TextChoices):
         ADMIN = "admin", "Admin"
@@ -9,6 +28,7 @@ class User(AbstractUser):
 
     role = models.CharField(choices=Roles.choices, max_length=20,
                             default=Roles.CUSTOMER)
+    objects = CustomUserManager()
 
     def is_admin(self):
         return self.role == self.Roles.ADMIN
